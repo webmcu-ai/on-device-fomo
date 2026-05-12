3bcb8bfd8e412be87cb91de23613a3a5264b712d
Jeremy Ellis <keyfreemusic@gmail.com>
2026-05-11 22:31:12 -0700

    Update pwa.js

--- pwa/pwa.js
+++ pwa/pwa.js
@@ -81,7 +81,7 @@ function myPwaUpdateBtn() {
 async function myHandleAppAction() {
     if (!myPwaIsPaid()) {
         // Send user to pwa/index.html, which knows how to return here
-        window.location.href = './pwa.html';
+        window.location.href = './pwa/pwa.html';
         return;
     }
 
